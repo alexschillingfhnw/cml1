4f07ac66a18544d9f3a8fee894908e9ae973bb46
mariuslae <91615393+mariuslae@users.noreply.github.com>
2023-12-08 12:28:34 +0100

    updated linreg streamlit

--- Berichte/2_Vorhersage_des_Immobilienpreises/2.3_Webservice/pages/simple_regression.py
+++ Berichte/2_Vorhersage_des_Immobilienpreises/2.3_Webservice/pages/simple_regression.py
@@ -5,41 +5,16 @@ from sklearn.linear_model import LinearRegression
 from scipy import stats
 import os
 import locale
+import pickle
 locale.setlocale(locale.LC_ALL, 'de_CH.UTF-8')
 
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+loaded_model = pickle.load(open('../../../99_gespeicherte_modelle/linreg_model.sav', 'rb'))
 
-def fit_linear_regression(X, y, transformation_func=None):
-    # Apply transformation if provided
-    if transformation_func:
-        X = transformation_func(X)
-    
-    # Train linear regression model
-    lin_reg = LinearRegression()
-    if len(X.shape) == 1:
-        lin_reg.fit(X.values.reshape(-1, 1), y)
-    else:
-        lin_reg.fit(X, y)
-    return lin_reg
 
 def predict(model, X):
-    if len(X.shape) == 1:
-        return model.predict(X.values.reshape(-1, 1))
-    else:
-        return model.predict(X)
-
-
-df = pd.read_csv('../../../../data/immo_data_clean.csv', low_memory=False)
-df = df[['Living_area_unified', 'Floor_space_merged','price_cleaned']]
-df = df.dropna(subset=['Living_area_unified','Floor_space_merged'], how='all')
-df['Living_area_unified'] = df['Living_area_unified'].fillna(df['Floor_space_merged'])
-z = np.abs(stats.zscore(df['Living_area_unified']))
-df_temp = df[(z < 18)]
-X34 = df['Living_area_unified']
-y34 = df['price_cleaned']
-model = fit_linear_regression(X34, y34, None)
-
+    return np.square(model.predict(np.sqrt(X)))
 # make title
 st.title('Immo-Calculator')
 
@@ -51,7 +26,7 @@ with st.form(key='my_form'):
     if submit_button:
         # make prediction
         X = pd.DataFrame({'Living_area_unified': [living_space]})
-        prediction = predict(model, X)
+        prediction = predict(loaded_model, X)
         prediction_local = locale.format_string('%.2f', prediction[0], True)
         st.subheader(f'Predicted Price: {prediction_local} CHF')
         
