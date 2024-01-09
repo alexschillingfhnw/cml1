0802510606a1fc2d2e14897c4537a91ed743d723
mariuslae <91615393+mariuslae@users.noreply.github.com>
2024-01-09 11:48:19 +0100

    webservice_update

--- Berichte/2_Vorhersage_des_Immobilienpreises/2.3_Webservice/home.py
+++ Berichte/2_Vorhersage_des_Immobilienpreises/2.3_Webservice/home.py
@@ -1,12 +1,11 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
-from sklearn.linear_model import LinearRegression
-from scipy import stats
-import os
-import locale
 
+# if in directory, start the app from terminal with: streamlit run ./home.py
 st.title('Immo-Calculator')
-st.header('Immo-Calculator')
 st.subheader('Instructions')
-st.text('Choose your model in the navigation bar based on your available information')
+st.write("""
+         Welcome to our Immo-Calculator. 
+         This is a simple tool to predict the price of a property.
+         Choose your model in the navigation bar based on your available information. 
+         The simple regression will only need the living space of the property. The expert model will need more information.
+         """)
--- Berichte/2_Vorhersage_des_Immobilienpreises/2.3_Webservice/pages/simple_regression.py
+++ Berichte/2_Vorhersage_des_Immobilienpreises/2.3_Webservice/pages/simple_regression.py
@@ -1,32 +1,38 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from sklearn.linear_model import LinearRegression
-from scipy import stats
 import os
 import locale
 import pickle
+
+# set locale to german (Switzerland)
 locale.setlocale(locale.LC_ALL, 'de_CH.UTF-8')
 
-
+# change working directory so we can work with relative paths
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# load model
 loaded_model = pickle.load(open('../../../99_gespeicherte_modelle/linreg_model.sav', 'rb'))
 
-
+#small function to predict price
 def predict(model, X):
     return np.square(model.predict(np.sqrt(X)))
-# make title
+
+# Title
 st.title('Immo-Calculator')
-
-# make input fields
+# Instructions
+st.text("Enter Living Space of your Property in sqm")
+# Simple form
 with st.form(key='my_form'):
+    # Input
     living_space = st.number_input(label='SQM Living Space')
+    # Submit Button
     submit_button = st.form_submit_button('Submit')
-
+    # Prediction
     if submit_button:
-        # make prediction
         X = pd.DataFrame({'Living_area_unified': [living_space]})
         prediction = predict(loaded_model, X)
+        # Format prediction to CHF
         prediction_local = locale.format_string('%.2f', prediction[0], True)
+        # Print prediction
         st.subheader(f'Predicted Price: {prediction_local} CHF')
         
